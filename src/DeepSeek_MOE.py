f067f4324317ea6cbdcb1dc6bee6bf0f9164eb9f
aowei <1585545669@qq.com>
2026-09-12 15:27:04 +0800

    增加了简易版 DeepSeekMoE 实现和专家负载测试

--- src/DeepSeek_MOE.py
+++ src/DeepSeek_MOE.py
@@ -21,10 +21,10 @@ class TopkRouter(nn.Module):
         self.top_k = args.top_k
         self.add_noise = args.add_noise
 
-        self.gate_linear = nn.Linear(args.n_dim, args.n_expert)
+        self.gate_linear = nn.Linear(args.n_dim, args.n_experts)
 
         if args.add_noise:
-            self.noise_linear = nn.Linear(args.n_dim, args.n_expert)
+            self.noise_linear = nn.Linear(args.n_dim, args.n_experts)
 
     def forward(self, mha_output):
         logits = self.gate_linear(mha_output)
@@ -100,3 +100,84 @@ class SparseMOE(nn.Module):
                 final_output[expert_mask] += weighted_output
 
         return final_output 
+
+# 类 DeepSeek MoE（简化的路由机制和负载均衡）
+@dataclass
+class ModelArgs:
+    n_dim: int
+    n_experts: int
+    n_shared_experts: int
+    top_k: int
+    dropout: float
+    batch: int
+    seq_len: int
+    add_noise: bool
+
+args = ModelArgs(n_dim=32, n_experts=4, n_shared_experts=1, top_k=2, dropout=0.1, batch=1, seq_len=5, add_noise=True)
+
+class DeepSeekExpert(nn.Module):
+    def __init__(self, args: ModelArgs):
+        super().__init__()
+
+        self.gate = nn.Linear(args.n_dim, 4 * args.n_dim, bias=False)
+        self.value = nn.Linear(args.n_dim, 4 * args.n_dim, bias=False)
+        self.out = nn.Linear(4 * args.n_dim, args.n_dim, bias=False)
+
+    def forward(self, x):
+        return self.out(nn.functional.silu(self.gate(x)) * self.value(x))
+
+class DeepSeekMoE(nn.Module):
+    def __init__(self, args: ModelArgs):
+        super().__init__()
+
+        self.router = TopkRouter(args)
+        self.routed_experts = nn.ModuleList([DeepSeekExpert(args) for _ in range(args.n_experts)])
+        self.shared_experts = nn.ModuleList([DeepSeekExpert(args) for _ in range(args.n_shared_experts)])
+        self.top_k = args.top_k
+
+    def forward(self, x):
+        gating_output, indices = self.router(x) # [B, T, N] [B, T, K]
+        
+        final_output = torch.zeros_like(x) # [B, T, D]
+
+        for i, expert in enumerate(self.routed_experts):
+            expert_mask = (indices == i).any(dim=-1) # [B, T]
+
+            if expert_mask.any():
+                expert_input = x[expert_mask] # [selected_token, D]
+
+                expert_output = expert(expert_input) # [selected_token, D]
+
+                gating_scores = gating_output[..., i] # [B, T]
+                gating_scores = gating_scores[expert_mask] # [selected_token]
+                gating_scores = gating_scores.unsqueeze(1) # [selected_token, 1]
+
+                # [B, T, N] -> [selected_token, N] -> [selected_token] -> [selected_token, 1]
+                # 或者是：gating_scores = gating_output[expert_mask][:, i].unsqueeze(1)
+
+                weighted_output = gating_scores * expert_output
+
+                final_output[expert_mask] += weighted_output
+
+        for i, expert in enumerate(self.shared_experts):
+            final_output += expert(x)
+
+        return final_output, indices
+
+torch.manual_seed(666)
+import numpy as np
+
+deepseek_moe = DeepSeekMoE(args)
+
+count = 100
+experts_utilization = np.zeros(args.n_experts, dtype=int)
+for _ in range(count):
+    mha_output = torch.randn(args.batch, args.seq_len, args.n_dim)
+    _, indices = deepseek_moe(mha_output)
+    indices = indices.detach().cpu().numpy()
+    indices = indices.flatten()
+    for idx in indices:
+        experts_utilization[idx] += 1
+
+print("专家负载：", experts_utilization)
+print("激活的总次数：", experts_utilization.sum())
